fbe8c290169f7e57ea592f8c60cd50ec25c29e7b
jolivepetrus <jolive@iberoxarxa.com>
2017-04-04 17:20:58 +0200

    Broadcast, Broadcast and Wait, and Event is being processed?, may only be used within a hat block.

--- src/blocks/events.js
+++ src/blocks/events.js
@@ -87,7 +87,34 @@ Blockly.Blocks['broadcast'] = {
 		this.setColour(Blockly.Blocks.events.HUE);
 		this.setTooltip(Blockly.Msg.EVENT_BROADCAST_TOOLTIP);
 		this.setHelpUrl(Blockly.Msg.EVENT_BROADCAST_HELPURL);
-	}
+	},
+    onchange: function(e) {
+      if (!this.workspace.isDragging || this.workspace.isDragging()) {
+        return;
+      }
+      var legal = false;
+      // Is the block nested in a loop?
+      var block = this;
+      do {
+        if (this.SURROUND_TYPES.indexOf(block.type) != -1) {
+          legal = true;
+          break;
+        }
+        block = block.getSurroundParent();
+      } while (block);
+      if (legal) {
+        this.setWarningText(null);
+        if (!this.isInFlyout) {
+          this.setDisabled(false);
+        }
+      } else {
+        this.setWarningText('Warning: This block may only be used within a hat block.');
+        if (!this.isInFlyout && !this.getInheritedDisabled()) {
+          this.setDisabled(true);
+        }
+      }
+    },
+    SURROUND_TYPES: ['when_board_starts', 'when_i_receive', 'when_digital_pin', 'when_i_receive_a_lora_frame']
 };
 
 Blockly.Blocks['broadcast_and_wait'] = {
@@ -102,7 +129,34 @@ Blockly.Blocks['broadcast_and_wait'] = {
 		this.setColour(Blockly.Blocks.events.HUE);
 		this.setTooltip(Blockly.Msg.EVENT_BROADCAST_AND_WAIT_TOOLTIP);
 		this.setHelpUrl(Blockly.Msg.EVENT_BROADCAST_AND_WAIT_HELPURL);
-	}
+	},
+    onchange: function(e) {
+      if (!this.workspace.isDragging || this.workspace.isDragging()) {
+        return;
+      }
+      var legal = false;
+      // Is the block nested in a loop?
+      var block = this;
+      do {
+        if (this.SURROUND_TYPES.indexOf(block.type) != -1) {
+          legal = true;
+          break;
+        }
+        block = block.getSurroundParent();
+      } while (block);
+      if (legal) {
+        this.setWarningText(null);
+        if (!this.isInFlyout) {
+          this.setDisabled(false);
+        }
+      } else {
+        this.setWarningText('Warning: This block may only be used within a hat block.');
+        if (!this.isInFlyout && !this.getInheritedDisabled()) {
+          this.setDisabled(true);
+        }
+      }
+    },
+    SURROUND_TYPES: ['when_board_starts', 'when_i_receive', 'when_digital_pin', 'when_i_receive_a_lora_frame']
 };
 
 Blockly.Blocks['event_is_being_processed'] = {
@@ -118,5 +172,32 @@ Blockly.Blocks['event_is_being_processed'] = {
 		this.setColour(Blockly.Blocks.events.HUE);
 		this.setTooltip(Blockly.Msg.EVENT_IS_BEING_PROCESSED_TOOLTIP);
 		this.setHelpUrl(Blockly.Msg.EVENT_IS_BEING_PROCESSED_HELPURL);
-	}
+	},
+    onchange: function(e) {
+      if (!this.workspace.isDragging || this.workspace.isDragging()) {
+        return;
+      }
+      var legal = false;
+      // Is the block nested in a loop?
+      var block = this;
+      do {
+        if (this.SURROUND_TYPES.indexOf(block.type) != -1) {
+          legal = true;
+          break;
+        }
+        block = block.getSurroundParent();
+      } while (block);
+      if (legal) {
+        this.setWarningText(null);
+        if (!this.isInFlyout) {
+          this.setDisabled(false);
+        }
+      } else {
+        this.setWarningText('Warning: This block may only be used within a hat block.');
+        if (!this.isInFlyout && !this.getInheritedDisabled()) {
+          this.setDisabled(true);
+        }
+      }
+    },
+    SURROUND_TYPES: ['when_board_starts', 'when_i_receive', 'when_digital_pin', 'when_i_receive_a_lora_frame']
 };
